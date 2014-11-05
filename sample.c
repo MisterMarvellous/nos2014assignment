184c2c576994347210c754b4753ee0d3f287911a
Todd Fanning (fann0022) <fann0022@flinders.edu.au>
2014-11-05 18:56:56 +1030

    Send correct responses upon USER command and basic PRIVMSG functionality

--- sample.c
+++ sample.c
@@ -45,8 +45,8 @@ struct client_thread {
   char nickname[32];
 
   int state;
-#define DEAD 0
-#define ALIVE 1
+#define DEAD 1
+#define ALIVE 2
   int user_command_seen;
   int user_has_registered;
   time_t timeout;
@@ -63,7 +63,52 @@ struct client_thread threads[MAX_CLIENTS];
 // the number of connections we have open right now
 int client_count=0;
 
-pthread_rwlock_t message_log_lock;
+pthread_rwlock_t message_log_lock = PTHREAD_RWLOCK_INITIALIZER;
+
+#define MAX_MESSAGES 10000
+char *message_log_senders[MAX_MESSAGES];
+char *message_log_recipients[MAX_MESSAGES];
+char *message_log[MAX_MESSAGES];
+int message_count=0;
+
+int message_log_append(char *sender,
+		       char *recipient,
+		       char *message) {
+  if (message_count>=MAX_MESSAGES) return -1;
+  pthread_rwlock_wrlock(&message_log_lock);
+
+  // Append the message to the log here
+  message_log_senders[message_count]=strdup(sender);
+  message_log_recipients[message_count]=strdup(recipient);
+  message_log[message_count]=strdup(message);
+  message_count++;
+
+  pthread_rwlock_unlock(&message_log_lock);
+  return 0;
+}
+
+int message_log_read(struct client_thread *t) {
+  pthread_rwlock_rdlock(&message_log_lock);
+
+  // Read and process new messages in the log
+  int i;
+  for (i=t->next_message;i<message_count;++i) {
+    if (!strcasecmp(t->nickname, message_log_recipients[i])) {
+      // This message is for us
+      char msg[8192];
+      snprintf(msg, 8192, ":%s PRIVMSG %s :%s\n",
+	       message_log_senders[i],
+	       message_log_recipients[i],
+	       message_log[i]);
+      write(t->fd,msg,strlen(msg));
+    }
+  }
+  t->next_message=message_count;
+
+  pthread_rwlock_unlock(&message_log_lock);
+  return 0;
+
+}
 
 int read_from_socket(int sock,unsigned char *buffer,int *count,int buffer_size,
 		     int timeout)
@@ -141,7 +186,7 @@ void *client_thread_entry(void *arg) {
 
   printf("Client thread started\n");
   // run the thread stuffs
-  connection_main(t->fd);
+  connection_main(t);
   t->state=DEAD;
   return NULL;
 }
@@ -161,7 +206,7 @@ int handle_connection(int fd) {
   
   // ADD STRUCTURE CLEARING LINE HERE
   // MARK THE CONNECTION SLOTS AS FREE FOR REUSE WHEN A CONNECTION CLOSES
-  // memset(&threads[i],0,sizeof(struct client_thread));
+  bzero(&threads[i],sizeof(struct client_thread));
   
   threads[i].fd = fd;
   threads[i].state = ALIVE;
@@ -177,51 +222,126 @@ int handle_connection(int fd) {
   return 0;
 }
 
-int connection_main(int fd) {
-  printf("I have now seen %d connections.\n", ++connection_count);
+int registration_check(struct client_thread *t) {
+  if (t->user_has_registered) return -1;
+
+  if (t->user_command_seen&&t->nickname[0]) {
+    // User has now met the registration requirements
+    t->user_has_registered=1;
+    t->timeout=60;
+    char msg[8192];
+    snprintf(msg, 1024, ":toddsircserver.com 001 %s :You have registered. Hey there.\n", t->nickname);
+    write(t->fd,msg,strlen(msg));
+    snprintf(msg, 1024, ":toddsircserver.com 002 %s :More information to go here.\n", t->nickname);
+    write(t->fd,msg,strlen(msg));
+    snprintf(msg, 1024, ":toddsircserver.com 003 %s :This message intentionally left blank.\n", t->nickname);
+    write(t->fd,msg,strlen(msg));
+    snprintf(msg, 1024, ":toddsircserver.com 004 %s :Be generous with marks.\n", t->nickname);
+    write(t->fd,msg,strlen(msg));
+    snprintf(msg, 1024, ":toddsircserver.com 253 %s :??? unknown connections.\n", t->nickname);
+    write(t->fd,msg,strlen(msg));
+    snprintf(msg, 1024, ":toddsircserver.com 254 %s :??? channels formed.\n", t->nickname);
+    write(t->fd,msg,strlen(msg));
+    snprintf(msg, 1024, ":toddsircserver.com 255 %s :I have ??? clients and ??? servers.\n", t->nickname);
+    write(t->fd,msg,strlen(msg));
+    return 0;
+  }
+  return -1;
+}
+
+int parse_line(struct client_thread *t,char *buffer) {
   char msg[1024];
+  char channel[8192];
+  int r=sscanf((char *)buffer, "JOIN %s", channel);
+  if (r==1) {
+    if (!t->user_has_registered) {
+      snprintf(msg, 1024, ":toddsircserver.com 241 * :JOIN command sent before registration\n");
+      write(t->fd, msg, strlen(msg));
+    } 
+  }
+  char nickname[8192];
+  r=sscanf((char *)buffer, "NICK %s", nickname);
+  if (r==1) {
+    // Saw a nickname from the client
+    if (strlen(nickname)>30||strlen(nickname)<1) {
+      snprintf(msg,1024,"ERROR :Invalid nickname: Nickname too short or too long\n");
+      write(t->fd,msg,strlen(msg));
+    }
+    else {
+      // Nickname is a valid length
+      printf("Saw nickname '%s'\n",nickname);
+      strcpy(t->nickname,nickname);
+      registration_check(t);
+    }
+  }
+  if (!strncasecmp("USER ", (char *)buffer,5)) {
+    // Saw USER command. If a NICK has been provided, then mark connection
+    // as registered and send client greeting messages.
+    t->user_command_seen=1;
+    registration_check(t);
+  }
+  if (!strncasecmp("PRIVMSG", (char *)buffer, 7)) {
+    if (!t->user_has_registered) {
+      snprintf(msg, 1024, ":toddsircserver.com 241 * :PRIVMSG command sent before registration\n");
+      write(t->fd, msg, strlen(msg));
+    }
+    else {
+      // Accept and process PRIVMSG
+      char recipient[1024];
+      char message[1024];
+      if (sscanf((char *)buffer, "PRIVMSG %s :%[^\n]", recipient, message)==2) {
+	char sender[1024];
+	snprintf(sender,1024,"%s!myusername@myserver",t->nickname);
+	message_log_append(sender, recipient, message);
+      }
+      else {
+	// mal-formed PRIVMSG command, so complain
+	snprintf(msg, 1024, ":toddsircserver.com 461 %s :Mal-formed PRIVMSG command sent\n", t->nickname);
+	write(t->fd, msg, strlen(msg));
+      }
+    }
+  }
+  if (!strncasecmp("QUIT", (char *)buffer, 4)) {
+    // Client is leaving
+    // if we don't close connection, there will be a SIGPIPE.
+    // be afraid.
+    snprintf(msg, 1024, "ERROR :Closing Link: Client sent QUIT\n");
+    write(t->fd, msg, strlen(msg));
+    close(t->fd); t->fd=-1;
+    return 0;
+  }
+  
+}
+
+int connection_main(struct client_thread *t) {
+  printf("I have now seen %d connections.\n", ++connection_count);
+  char msg[8192];
   sprintf(msg, ":toddsircserver.com 020 * :Heyooooo\n");
-  write(fd, msg, strlen(msg));
+  write(t->fd, msg, strlen(msg));
 
   unsigned char buffer[8192];
   int length=0;
-  int registered=0;
+  t->timeout=5;
 
-  while(1) {
+  int time_of_last_data=time(0);
+  while(t->fd>=0) {
     length=0;
-    read_from_socket(fd, buffer, &length, 8192, 5);
-    if (length==0) {
+    message_log_read(t);
+    read_from_socket(t->fd, buffer, &length, 8192, 1);
+    if (length>0) time_of_last_data=time(0);
+    if (length==0&&((time(0)-time_of_last_data)>=t->timeout)) {
       snprintf(msg, 1024, "ERROR :Closing Link: Connection timeout\n");
-      write(fd, msg, strlen(msg));
-      close(fd);
+      write(t->fd, msg, strlen(msg));
+      close(t->fd);
       return 0;
     }
-    
-    char channel[8192];
-    int r=sscanf((char *)buffer, "JOIN %s", channel);
-    if (!registered) {
-      if (r==1) {
-	snprintf(msg, 1024, ":toddsircserver.com 241 * :JOIN command sent before registration\n");
-	write(fd, msg, strlen(msg));
-      }
-      if (!strncasecmp("PRIVMSG", (char *)buffer, 7)) {
-	snprintf(msg, 1024, ":toddsircserver.com 241 * :PRIVMSG command sent before registration\n");
-	write(fd, msg, strlen(msg));
-      }
-    }
-    if (!strncasecmp("QUIT", (char *)buffer, 4)) {
-      // Client is leaving
-      // if we don't close connection, there will be a SIGPIPE.
-      // be afraid.
-      snprintf(msg, 1024, "ERROR :Closing Link: Client sent QUIT\n");
-      write(fd, msg, strlen(msg));
-      close(fd);
-      return 0;
-    }
-  }
-  
 
-  close(fd);
+    buffer[length]=0;
+
+    parse_line(t,(char *)buffer);
+  }
+
+  close(t->fd);
   return 0;
 }
 
